43daaa3c0545a2d99b5af795cf9a24c90d155ac6
Manfi <mahmoud.manfy159@gmail.com>
2020-04-10 22:10:15 +0200

    The first

---
+++ src/eg/edu/alexu/csd/filestructure/redblacktree/Main.java
@@ -0,0 +1,11 @@
+package eg.edu.alexu.csd.filestructure.redblacktree;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+
+    }
+
+}
